5a0df70f56064a475627d4120811c53123756cb2
Christopher Skoblenick <irworkinghard@gmail.com>
2018-11-12 00:41:47 -0500

    configured very basic webpack server setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+// Make a div.
+const messageElement = document.createElement('div');
+
+// Put something in the div.
+messageElement.textContent = "It's working!!!";
+
+// Add to the end of the document.
+document.body.appendChild(messageElement);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// Used snowbillr.github.io/blog/2018-04-09-a-modern-web-development-setup-for-phaser-3/ to assist in creation of this.
+
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, 'src/'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env']
+                    }
+                }
+            }
+        ]
+    },
+
+    // This is for the webpack-dev-server.
+    // Serve the contents of the build folder.
+    devServer: {
+        contentBase: path.resolve(__dirname, 'build')
+    },
+
+    plugins: [
+        // Copy a file into output directory when we build.
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, 'src/index.html'),
+                to: path.resolve(__dirname, 'build')
+            }
+        ])
+    ]
+};
